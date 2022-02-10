99b75e3264d63b41f2043d06861a7de46d66ee65
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2022-02-10 20:08:15 +0600

    navbar with banner design

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,55 @@
+import { Box, Container, Grid } from "@mui/material";
 import React from "react";
 import "./Banner.css";
 
 const Banner = () => {
-  return <div>
-      
-  </div>;
+  return (
+    <div className="bg banner">
+      <Container>
+        <Grid container spacing={2} columns={16}>
+          <Grid sx={{ display: "flex", alignItems: "center" }} sm={16} lg={8} xs={16}>
+            <Box>
+              <h2 className="banner-content1">
+                The Best Course To Achieve Your <span className="banner-content-secondary">Digital Marketplace</span>
+              </h2>
+              <p className="banner-content2">
+                We make selling digital products a breeze. Easy Digital Downloads is simple to learn and free to download.
+              </p>
+              <Box sx={{ display: "flex" }}>
+                {/* first button */}
+
+                <button class="cssbuttons-io-button">
+                  {" "}
+                  Get started now
+                  <div class="icon">
+                    <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M0 0h24v24H0z" fill="none"></path>
+                      <path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path>
+                    </svg>
+                  </div>
+                </button>
+
+                {/* 2nd button */}
+                <button class="cta">
+                  <span> Explore courses</span>
+                  <svg width="15px" height="10px" viewBox="0 0 13 10">
+                    <path d="M1,5 L11,5"></path>
+                    <polyline points="8 1 12 5 8 9"></polyline>
+                  </svg>
+                </button>
+              </Box>
+            </Box>
+          </Grid>
+
+          <Grid sm={16} lg={8} xs={16}>
+            <Box>
+              <img className="banner-area" src="https://i.ibb.co/QrFFyx2/b8ae0dc98044f8c4c4b2cb236d2540b5.png" alt="" />
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
 };
 
 export default Banner;
--- src/Pages/Home/Navbar/Navbar.js
+++ src/Pages/Home/Navbar/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div>
       {/* Mui-navbar package used */}
-      <MuiNavbar logo="https://i.ibb.co/N3Y9VVw/logo.png" background="#444444">
+      <MuiNavbar logo="https://i.ibb.co/N3Y9VVw/logo.png" background="#00ACD6">
         <NavItem to="/home">HOME</NavItem>
         <NavItem to="/about">ABOUT US</NavItem>
         <NavItem to="/course">OUR COURSES</NavItem>
