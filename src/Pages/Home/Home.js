f235973e01e08639b477d1e1dcb1310f6a2e9f8d
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2022-02-14 09:09:24 +0600

    added workflow area

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./Banner/Banner";
+import Workflow from "./Workflow/Workflow";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Workflow />
     </div>
   );
 };
