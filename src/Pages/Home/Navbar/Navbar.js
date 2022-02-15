c3c500baa0d12b126729afedab885fe58e97c0ea
rokibulslam <rokibulslam6@gmail.com>
2022-02-15 14:31:39 +0600

    display name at navbar

--- src/Pages/Home/Navbar/Navbar.js
+++ src/Pages/Home/Navbar/Navbar.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { MuiNavbar, NavItem } from "mui-navbar";
 import MuiButton from "../../../StyledComponent/MuiButton";
-
+import useAuth from "../../../Hooks/useAuth";
+import profile from "../../../Image/profile.png"
 const Navbar = () => {
+    const {user} = useAuth()
   return (
     <div>
       {/* Mui-navbar package used */}
@@ -11,9 +13,26 @@ const Navbar = () => {
         <NavItem to="/about">ABOUT US</NavItem>
         <NavItem to="/course">OUR COURSES</NavItem>
         <NavItem to="/contact">CONTACT</NavItem>
-        <NavItem to="/login">
-          <MuiButton sx={{ color: "white", padding: "5px 30px", fontSize: "18px" }}>Login</MuiButton>
-        </NavItem>
+        {user.email ? (
+          <NavItem to="/login">
+            <MuiButton
+              sx={{ color: "white", padding: "5px 30px", fontSize: "18px" }}
+            >
+              Login
+            </MuiButton>
+          </NavItem>
+        ) : (
+          <NavItem to="/login">
+            <MuiButton
+              sx={{ color: "white", padding: "5px 30px", fontSize: "18px" }}
+            >
+              Logout
+            </MuiButton>
+          </NavItem>
+        )}
+        <NavItem>{user.displayName}</NavItem>
+        
+        
       </MuiNavbar>
     </div>
   );
