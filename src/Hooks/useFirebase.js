3f5047812062a772485426c037ae86ca885bb9a3
rokibulslam <rokibulslam6@gmail.com>
2022-02-15 13:11:09 +0600

    register complete

--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -1,20 +1,31 @@
+import { useEffect, useState } from "react";
 
-import React, { useEffect, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, updateProfile } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import initializeFirebase from "../Firebase/Firebase.init";
 
-import initializeFirebase from '../Firebase/Firebase.init';
-
-
+// Initialize firebase
 
 initializeFirebase();
+
 const useFirebase = () => {
   const [user, setUser] = useState({});
-  const [isLoadin, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [authError, setAuthError] = useState("");
+  const [admin, setAdmin] = useState(false);
+  
   const auth = getAuth();
   const googleProvider = new GoogleAuthProvider();
 
-  //  Google SignIn
+  // Google SignIn
   const signInWithGoogle = (location, navigate) => {
     setIsLoading(true);
     signInWithPopup(auth, googleProvider)
@@ -22,6 +33,8 @@ const useFirebase = () => {
         const user = result?.user;
         setUser(user);
 
+        // Save user to database
+        saveUserData(user?.email, user?.displayName, "PUT");
         setAuthError("");
         const destination = location?.state?.from || "/dashboard";
         navigate(destination);
@@ -31,31 +44,45 @@ const useFirebase = () => {
       })
       .finally(() => setIsLoading(false));
   };
-  const register = (name, password, email, navigation) => {
+  // Register New User
+  const registerUser = (email, password, name, navigate) => {
+    setIsLoading(true);
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         setAuthError("");
         const newUser = { email, displayName: name };
-        // Set User to state
         setUser(newUser);
+        // save user to Database
+        saveUserData(email, name, "POST");
         // send name to firebase after creation
         updateProfile(auth.currentUser, {
           displayName: name,
         })
           .then(() => {})
-          .catch((error) => {
-            setAuthError(error.message);
-          });
-        navigation("/");
+          .catch((error) => {});
+        navigate("/");
       })
       .catch((error) => {
         setAuthError(error.message);
       })
-      .finally(() => {
-        setIsLoading(false);
-      });
+      .finally(() => setIsLoading(false));
   };
-  // Get Real Time User Details and Observe User's State
+  // Login User
+  const loginUser = (email, password, location, navigate) => {
+    setIsLoading(true);
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const destination = location?.state?.from || "/dashboard";
+        navigate(destination);
+        setAuthError("");
+      })
+      .catch((error) => {
+        setAuthError(error.message);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  // Observe User's State
   useEffect(() => {
     const unsubscribed = onAuthStateChanged(auth, (user) => {
       if (user) {
@@ -68,7 +95,45 @@ const useFirebase = () => {
     return () => unsubscribed;
   }, [auth]);
 
-  return { signInWithGoogle };
-};
+  // Logout User
+  const logout = () => {
+    setIsLoading(true);
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      })
+      .finally(() => setIsLoading(false));
+  };
+  // Get user role from database
+  useEffect(() => {
+    fetch()
+      .then((res) => res.json())
+      .then((data) => setAdmin(data.admin));
+  }, [user?.email]);
+  // Save user to database
+  const saveUserData = (email, name, method) => {
+    const userData = { email, name };
+    fetch('', {
+      method: method,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    }).then();
+  };
 
+  return {
+    user,
+    isLoading,
+    signInWithGoogle,
+    logout,
+    authError,
+    registerUser,
+    loginUser,
+    admin,
+  };
+};
 export default useFirebase;
--- src/Pages/Shared/Register/Register.js
+++ src/Pages/Shared/Register/Register.js
@@ -4,20 +4,27 @@ import useAuth from '../../../Hooks/useAuth'
 import "./Register.css";
 
 const Register = () => {
-  const [userRegisterData, setUserRegisterData] = useState();
-
+  const [userRegisterData, setUserRegisterData] = useState({});
   const navigate = useNavigate();
-  const location = useLocation();
-  const {} = useAuth();
-  console.log(userRegisterData)
-  // Handling Register form input field
+  const { user, registerUser, isLoading, authError } = useAuth();
+
+  // Handling input field
   const handleOnBlur = (e) => {
-    const name = e.target.name;
+    const nameField = e.target.name;
     const value = e.target.value;
-    // copy previous register data from state
-    const registerData = { ...userRegisterData };
-    registerData[name] = value;
-    setUserRegisterData(registerData);
+    const finalRegisterData = { ...userRegisterData };
+    finalRegisterData[nameField] = value;
+    setUserRegisterData(finalRegisterData);
+  };
+  // handle submit button for register
+  const handleRegisterSubmit = (e) => {
+    if (userRegisterData.password !== userRegisterData.password2) {
+      alert("Your password did not match");
+      return;
+    }
+    //   Sending parameter to firebase hook for registering user
+    registerUser(userRegisterData.email,userRegisterData.password, userRegisterData.name, navigate);
+    e.preventDefault();
   };
   
 
@@ -29,7 +36,7 @@ const Register = () => {
         </div>
 
         <div class="heading">Create A New Account</div>
-        <div class="input-text">
+        <form onSubmit={handleRegisterSubmit} class="input-text">
           <div class="username">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
               <path d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z" />
@@ -73,14 +80,13 @@ const Register = () => {
             <input
               onClick={handleOnBlur}
               type="password"
-              name="password"
+              name="password2"
               class="pass-input"
               placeholder="confirm password*"
             />
           </div>
-        </div>
-
-        <button class="sign-btn2">SIGNUP</button>
+          <button type="submit" class="sign-btn2">SIGNUP</button>
+        </form>
 
         <div class="link">
           Already have an account? <NavLink to="/login">Sign in</NavLink>
