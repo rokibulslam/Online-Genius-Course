6206ef9e8ec1cf37a7608bad5aef8aead905386e
rokibulslam <rokibulslam6@gmail.com>
2022-02-15 12:02:14 +0600

    user with email password & observer added

--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -1,6 +1,6 @@
 
-import React, { useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import React, { useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, updateProfile } from "firebase/auth";
 
 import initializeFirebase from '../Firebase/Firebase.init';
 
@@ -8,20 +8,20 @@ import initializeFirebase from '../Firebase/Firebase.init';
 
 initializeFirebase();
 const useFirebase = () => {
-    const [user, setUser] = useState({})
-    const [isLoadin, setIsLoading] = useState(false)
-    const [authError, setAuthError] = useState('')
-    const auth = getAuth()
-    const googleProvider = new GoogleAuthProvider();
-    
-    //  Google SignIn
+  const [user, setUser] = useState({});
+  const [isLoadin, setIsLoading] = useState(false);
+  const [authError, setAuthError] = useState("");
+  const auth = getAuth();
+  const googleProvider = new GoogleAuthProvider();
+
+  //  Google SignIn
   const signInWithGoogle = (location, navigate) => {
     setIsLoading(true);
     signInWithPopup(auth, googleProvider)
       .then((result) => {
         const user = result?.user;
         setUser(user);
-        
+
         setAuthError("");
         const destination = location?.state?.from || "/dashboard";
         navigate(destination);
@@ -32,17 +32,43 @@ const useFirebase = () => {
       .finally(() => setIsLoading(false));
   };
   const register = (name, password, email, navigation) => {
-    createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
-      setAuthError("")
-
-      const newUser = userCredential.user
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setAuthError("");
+        const newUser = { email, displayName: name };
+        // Set User to state
+        setUser(newUser);
+        // send name to firebase after creation
+        updateProfile(auth.currentUser, {
+          displayName: name,
+        })
+          .then(() => {})
+          .catch((error) => {
+            setAuthError(error.message);
+          });
+        navigation("/");
+      })
+      .catch((error) => {
+        setAuthError(error.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+  // Get Real Time User Details and Observe User's State
+  useEffect(() => {
+    const unsubscribed = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser({});
+      }
+      setIsLoading(false);
     });
-  }
-    
-    
-    
-    
-    return { signInWithGoogle };
+    return () => unsubscribed;
+  }, [auth]);
+
+  return { signInWithGoogle };
 };
 
 export default useFirebase;
