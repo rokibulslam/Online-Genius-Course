fe5e6c4afb64800a065bd364d6162983638b2fba
sumaiyatasnim <tasnimsumaiya07@gmail.com>
2022-02-17 23:55:02 +0600

    added footer design

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "../Footer/Footer";
 import Banner from "./Banner/Banner";
 import Cardd from "./Card/Cardd";
 import CourseDetails from "./CourseDetails/CourseDetails";
@@ -9,6 +10,7 @@ const Home = () => {
       <Banner />
       <Cardd></Cardd>
       <CourseDetails></CourseDetails>
+      <Footer></Footer>
     </div>
   );
 };
